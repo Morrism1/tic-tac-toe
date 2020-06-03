7c731af8905fa185e6f1ff66b6dff58b239f20ea
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2020-06-03 04:20:57 -0600

    get the users information

---
+++ lib/board.rb
@@ -0,0 +1,16 @@
+class Board
+    attr_writer
+    attr_reader :board
+
+    def initialize(board)
+        @board = board
+    end
+
+    def self.display_board(board)
+        puts "#{board[0]} | #{board[1]} | #{board[2]}"
+        puts '---------'
+        puts "#{board[3]} | #{board[4]} | #{board[5]}"
+        puts '---------'
+        puts "#{board[6]} | #{board[7]} | #{board[8]}"
+    end
+end

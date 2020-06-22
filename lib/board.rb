1e44a67034140cfc4dcc54b8a3cf1d54677077a9
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2020-06-22 02:32:01 -0600

    fixed stickler errors

--- lib/board.rb
+++ lib/board.rb
@@ -1,32 +1,32 @@
 class Board
-    attr_writer
-    attr_reader :board
+  attr_writer
+  attr_reader :board
 
-    def initialize(board)
-        @board = board
-    end
+  def initialize(board)
+    @board = board
+  end
 
-    def self.display_board(board)
-        puts "#{board[0]} | #{board[1]} | #{board[2]}"
-        puts '---------'
-        puts "#{board[3]} | #{board[4]} | #{board[5]}"
-        puts '---------'
-        puts "#{board[6]} | #{board[7]} | #{board[8]}"
-    end
+  def self.display_board(board)
+    puts "#{board[0]} | #{board[1]} | #{board[2]}"
+    puts '---------'
+    puts "#{board[3]} | #{board[4]} | #{board[5]}"
+    puts '---------'
+    puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  end
 
-    def self.player_move(player_selection)
-        player_selection = ""
-        while player_selection.empty?
-            player_selection = gets.chomp
-            if player_selection =~ /[1-9]/
-                player_selection
-            else
-                puts "select a valid move!"
-                player_selection = ""
-            end
-        end
-        player_selection_to_i = player_selection.to_i
-        player_move = player_selection_to_i - 1
-        return player_move
+  def self.player_move(player_selection)
+    player_selection = ''
+    while player_selection.empty?
+      player_selection = gets.chomp
+      if player_selection =~ /[1-9]/
+        player_selection
+      else
+        puts 'select a valid move!'
+        player_selection = ''
+      end
     end
+    player_selection_to_i = player_selection.to_i
+    player_move = player_selection_to_i - 1
+    player_move
+  end
 end
--- lib/validation.rb
+++ lib/validation.rb
@@ -1,44 +1,43 @@
-require_relative "./board.rb"
+require_relative './board.rb'
 class Logic
-
-    def self.valid_cell?(player_move, board)
-        case player_move
-        when 0..8
-            return true
-        else
-            return false
-        end
+  def self.valid_cell?(player_move, _board)
+    case player_move
+    when 0..8
+      true
+    else
+      false
     end
+  end
 
-    def self.empty_cell?(player_move, board)
-        board_data = board[player_move] 
-        case board_data
-        when 1..9
-            return true
-        else
-            return false
-        end
+  def self.empty_cell?(player_move, board)
+    board_data = board[player_move]
+    case board_data
+    when 1..9
+      true
+    else
+      false
     end
+  end
 
-    def self.game_defined?(board)
-        if board[0] == board[1] && board[1] == board[2]
-            return true
-        elsif board[3] == board[4] && board[4]== board[5]
-            return true
-        elsif board[6] == board[7] && board[7] == board[8]
-            return true
-        elsif board[0] == board[3] && board[3] == board[6]
-            return true
-        elsif board[1] == board[4] && board[4] == board[7]
-            return true
-        elsif board[2] == board[5] && board[5] == board[8]
-            return true
-        elsif board[0] == board[4] && board[4] == board[8]
-            return true
-        elsif board[2] == board[4] && board[4] == board[6]
-            return true
-        else
-            return false
-        end
+  def self.game_defined?(board)
+    if board[0] == board[1] && board[1] == board[2]
+      true
+    elsif board[3] == board[4] && board[4] == board[5]
+      true
+    elsif board[6] == board[7] && board[7] == board[8]
+      true
+    elsif board[0] == board[3] && board[3] == board[6]
+      true
+    elsif board[1] == board[4] && board[4] == board[7]
+      true
+    elsif board[2] == board[5] && board[5] == board[8]
+      true
+    elsif board[0] == board[4] && board[4] == board[8]
+      true
+    elsif board[2] == board[4] && board[4] == board[6]
+      true
+    else
+      false
     end
+  end
 end

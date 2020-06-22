36e3ea017f849f325985c104f618dd0a4f6d9353
Morrism1 <morrismontana0@gmail.com>
2020-06-22 16:49:04 +0200

    Remove puts from board class

--- lib/board.rb
+++ lib/board.rb
@@ -1,17 +1,18 @@
 class Board
-  attr_writer
-  attr_reader :board
+  attr_accessor :board
 
   def initialize(board)
     @board = board
   end
 
-  def self.display_board(board)
-    puts "#{board[0]} | #{board[1]} | #{board[2]}"
-    puts '---------'
-    puts "#{board[3]} | #{board[4]} | #{board[5]}"
-    puts '---------'
-    puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  def self.display_board
+    <<~MLS
+    #{@board[0]} | #{@board[1]} | #{@board[2]}
+   ---+---+---
+    #{@board[3]} | #{@board[4]} | #{@board[5]}
+   ---+---+---
+    #{@board[6]} | #{@board[7]} | #{@board[8]}
+ MLS
   end
 
   def self.player_move(player_selection)

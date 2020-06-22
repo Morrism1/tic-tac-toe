52af8f7b245e59a4226117805925bce8fc924d8f
Morrism1 <morrismontana0@gmail.com>
2020-06-22 16:58:55 +0200

    Linter issues

--- lib/board.rb
+++ lib/board.rb
@@ -7,11 +7,11 @@ class Board
 
   def self.display_board
     <<~MLS
-    #{@board[0]} | #{@board[1]} | #{@board[2]}
-   ---+---+---
-    #{@board[3]} | #{@board[4]} | #{@board[5]}
-   ---+---+---
-    #{@board[6]} | #{@board[7]} | #{@board[8]}
- MLS
+       #{@board[0]} | #{@board[1]} | #{@board[2]}
+      ---+---+---
+       #{@board[3]} | #{@board[4]} | #{@board[5]}
+      ---+---+---
+       #{@board[6]} | #{@board[7]} | #{@board[8]}
+    MLS
   end
 end

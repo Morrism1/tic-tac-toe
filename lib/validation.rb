8d441b09e907f11aa81dc125417850b48ad688d5
Morrism1 <morrismontana0@gmail.com>
2020-06-23 10:37:29 +0200

    Fix rubocop errors

--- lib/validation.rb
+++ lib/validation.rb
@@ -18,20 +18,20 @@ class Logic
     end
   end
 
-  def self.wining_row?(board)
+  def self.wining_row?(board) # rubocop:todo Metrics/PerceivedComplexity
     if board[0] == board[1] && board[1] == board[2] ||
        board[3] == board[4] && board[4] == board[5] ||
-       board[6] == board[7] && board[7] == board[8] 
+       board[6] == board[7] && board[7] == board[8]
       true
     else
       false
     end
   end
 
-  def self.wining_column?(board)
+  def self.wining_column?(board) # rubocop:todo Metrics/PerceivedComplexity
     if board[0] == board[3] && board[3] == board[6] ||
        board[1] == board[4] && board[4] == board[7] ||
-       board[2] == board[5] && board[5] == board[8] 
+       board[2] == board[5] && board[5] == board[8]
       true
     else
       false

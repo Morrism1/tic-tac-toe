fcac03be2aab9e37a9a5fe0c2cfae30346f7dbe0
tacodtripe <55043949+tacodtripe@users.noreply.github.com>
2020-06-23 02:09:54 -0600

    disabled rubocop Metrics in game_defined?

--- lib/validation.rb
+++ lib/validation.rb
@@ -19,14 +19,15 @@ class Logic
   end
 
   def self.game_defined?(board)
+    # rubocop:disable Metrics/PerceivedComplexity,Metrics/CyclomaticComplexity
     if board[0] == board[1] && board[1] == board[2] ||
-    board[3] == board[4] && board[4] == board[5] ||
-    board[6] == board[7] && board[7] == board[8] ||
-    board[0] == board[3] && board[3] == board[6] ||
-    board[1] == board[4] && board[4] == board[7] ||
-    board[2] == board[5] && board[5] == board[8] ||
-    board[0] == board[4] && board[4] == board[8] ||
-    board[2] == board[4] && board[4] == board[6]
+       board[3] == board[4] && board[4] == board[5] ||
+       board[6] == board[7] && board[7] == board[8] ||
+       board[0] == board[3] && board[3] == board[6] ||
+       board[1] == board[4] && board[4] == board[7] ||
+       board[2] == board[5] && board[5] == board[8] ||
+       board[0] == board[4] && board[4] == board[8] ||
+       board[2] == board[4] && board[4] == board[6]
       true
     else
       false

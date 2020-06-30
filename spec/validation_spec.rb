1e5cfdc251a8bf2bc6006e945fc00557a1498c1e
Morrism1 <morrismontana0@gmail.com>
2020-06-30 14:26:48 +0200

    Add tests for winning columns

--- spec/validation_spec.rb
+++ spec/validation_spec.rb
@@ -3,7 +3,7 @@ require './lib/validation.rb'
 require './lib/board.rb'
 
 RSpec.describe Logic do
-    let(:board) { Board.new([1, 2, 3, 4, 5, 6, 7, 8, 9]) } 
+    let(:board) { Board.new(['b', 'b', 'b', 4, 5, 6, 7, 8, 9]) } 
     describe 'valid_cell' do
         random_range = rand(0..8)
         it 'return true if player move is in range of 0 and 8' do
@@ -23,11 +23,38 @@ RSpec.describe Logic do
         end
     end
 
-    describe 'valid_cell' do
+    describe 'empty_cell' do
         it 'return true if board cell data is not in range of 1 and 9' do
             expect(Logic.empty_cell?(0,2)).to be(false)
         end
     end
 
+    describe 'wining_row' do
+        it 'return true if row of the cell is winning' do
+            board = ['b', 'b', 'b', 4, 5, 6, 7, 8, 9]
+            expect(Logic.wining_row?(board)).to be(true)
+        end
+    end
+
+    describe 'wining_row' do
+        it 'return false if row of the cell is not winning' do
+            board = ['b', 'a', 'b', 4, 5, 6, 7, 8, 9]
+            expect(Logic.wining_row?(board)).to_not be(true)
+        end
+    end
+
+    describe 'wining_column' do
+        it 'return true if column of the cell is winning' do
+            board = ['a', 2, 3, 'a', 5, 6, 'a', 8, 9]
+            expect(Logic.wining_column?(board)).to be(true)
+        end
+    end
+
+    describe 'wining_column' do
+        it 'return false if column of the cell is not winning' do
+            board = ['a', 2, 3, 'b', 5, 6, 'a', 8, 9]
+            expect(Logic.wining_column?(board)).to_not be(true)
+        end
+    end
 
 end
